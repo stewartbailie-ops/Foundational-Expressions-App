e3038ffc9da8c067c556050c0d900b9007a7d800
stewartbailie-ops <stewart.bailie@gmail.com>
2026-07-13 02:42:02 +0200

    Restore account setup notifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,22 +7,24 @@ import AdvisorPanel from "@/pages/AdvisorPanel";
 import AdvisorProfile from "@/pages/AdvisorProfile";
 import CallbackForm from "@/pages/CallbackForm";
 import ReferralForm from "@/pages/ReferralForm";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <Switch>
-        <Route path="/control">{() => <AdvisorPanel forcedSlug="erika" />}</Route>
-        <Route path="/advisor/:slug">{() => <AdvisorPanel />}</Route>
-        <Route path="/request-callback" component={FoundationalCallback} />
-        <Route path="/erika/request-callback" component={FoundationalCallback} />
-        <Route path="/erika" component={FoundationalProfile} />
-        <Route path="/:slug/request-callback" component={CallbackForm} />
-        <Route path="/:slug/referrals" component={ReferralForm} />
-        <Route path="/:slug" component={AdvisorProfile} />
-        <Route path="/" component={FoundationalProfile} />
-        <Route component={FoundationalProfile} />
-      </Switch>
+          <Route path="/control">{() => <AdvisorPanel forcedSlug="erika" />}</Route>
+          <Route path="/advisor/:slug">{() => <AdvisorPanel />}</Route>
+          <Route path="/request-callback" component={FoundationalCallback} />
+          <Route path="/erika/request-callback" component={FoundationalCallback} />
+          <Route path="/erika" component={FoundationalProfile} />
+          <Route path="/:slug/request-callback" component={CallbackForm} />
+          <Route path="/:slug/referrals" component={ReferralForm} />
+          <Route path="/:slug" component={AdvisorProfile} />
+          <Route path="/" component={FoundationalProfile} />
+          <Route component={FoundationalProfile} />
+        </Switch>
     </QueryClientProvider>
   );
 }
